bb35d538624aaa76dc8521b7250d632b3abee743
Jacob Boomgaarden <jcboomgaarden@gmail.com>
2015-09-20 14:36:58 -0500

    added classes folder with plugin.php

---
+++ classes/Plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Quixotical
+ * Date: 9/20/15
+ * Time: 2:32 PM
+ */
+namespace ElegantSlider;
+
+use WordWrap\LifeCycle;
+
+class Plugin extends LifeCycle{
+    
+}
