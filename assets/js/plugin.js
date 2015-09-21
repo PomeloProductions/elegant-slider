dca0ae7f01b33fa27b2d4a6071060206a0440bf4
Jacob Boomgaarden <jcboomgaarden@gmail.com>
2015-09-20 19:17:58 -0500

    added auto & pause elements to bxSlider function

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -2,5 +2,8 @@
  * Created by Quixotical on 9/20/15.
  */
 jQuery(document).ready(function(){
-    jQuery('.bxslider').bxSlider();
+    jQuery('.bxslider').bxSlider({
+        'auto' : {{auto_play}},
+        'pause' : {{auto_play_speed}}
+    });
 });
