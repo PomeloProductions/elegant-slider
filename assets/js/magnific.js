3d4a365aad4cbc39235d94470b048ba71456edff
Bryce Meyer <bryce@axolotlinteractive.com>
2015-10-22 01:27:44 -0500

    added function for loading popup

--- assets/js/magnific.js
+++ assets/js/magnific.js
@@ -1,3 +1,11 @@
 /**
  * Created by bryce on 10/22/15.
  */
+
+$(document).ready(function() {
+    $('.bxslider').magnificPopup({
+        delegate: 'img', // child items selector, by clicking on it popup will open
+        type: 'image'
+        // other options
+    });
+});
