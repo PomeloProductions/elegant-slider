4dbf62c078047329d2ee95f3ae77edf0c1616a93
Bryce Meyer <bryce@axolotlinteractive.com>
2015-10-22 00:12:08 -0500

    added js templates

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -5,8 +5,8 @@ jQuery(document).ready(function(){
     jQuery('.bxslider').bxSlider({
         'auto' : {{auto_play}},
         'pause' : {{auto_play_speed}},
-        'touchEnabled' : true,
-        'oneToOneTouch' : true,
-        'pager' : false
+        'touchEnabled' : {{fluid_touch}},
+        'oneToOneTouch' : {{fluid_touch}},
+        'pager' : {{pager}}
     });
 });
