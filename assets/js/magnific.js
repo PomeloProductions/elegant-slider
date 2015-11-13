7b5343e341df9df3df17dab769fa2d482a15f5c4
Bryce Meyer <bryce@axolotlinteractive.com>
2015-11-13 13:46:07 -0600

    changed title source to elminiate tooltip

--- assets/js/magnific.js
+++ assets/js/magnific.js
@@ -6,6 +6,9 @@ jQuery(document).ready(function() {
     jQuery('.bxslider#{{id}}').magnificPopup({
         delegate: 'img', // child items selector, by clicking on it popup will open
         type: 'image',
+        image: {
+            titleSrc: 'data-title'
+        },
         gallery: {
             enabled: true
         }
