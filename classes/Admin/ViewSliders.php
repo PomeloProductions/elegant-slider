16099a64b80e1c001ff92831c02e5bfe05396ab6
Bryce Meyer <bryce@axolotlinteractive.com>
2016-01-15 13:48:15 -0600

    added first build of viewing sliders

---
+++ classes/Admin/ViewSliders.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bryce
+ * Date: 1/15/16
+ * Time: 12:27 PM
+ */
+namespace ElegantSlider\Admin;
+
+use WordWrap\Admin\TaskController;
+use WordWrap\Assets\Template\Mustache\MustacheFactory;
+use WordWrap\Assets\Template\Mustache\MustacheTemplate;
+
+class ViewSliders extends TaskController {
+
+    /**
+     * override this to setup anything that needs to be done before
+     * @param $action string the action the user is trying to complete
+     */
+    public function processRequest($action = null) {
+        // TODO: Implement processRequest() method.
+    }
+
+    /**
+     * override to render the main page
+     */
+    protected function renderMainContent() {
+        $template = new MustacheTemplate($this->lifeCycle, "admin/list_sliders");
+
+        return $template->export();
+    }
+
+    /**
+     * override to render the main page
+     */
+    protected function renderSidebarContent() {
+        // TODO: Implement renderSidebarContent() method.
+    }
+}
