44964bacad6dd8f57c90a1d372fe02925d602bc5
Bryce Meyer <bryce13950@gmail.com>
2017-01-24 12:25:48 -0600

    added php version to config

--- elegant-slider.php
+++ elegant-slider.php
@@ -14,7 +14,7 @@ namespace ElegantSlider;
 
 use WordWrap;
 
-function has_word_wrap() {
+function hasWordWrap() {
     if ( is_admin() && current_user_can( 'activate_plugins' ) &&  !is_plugin_active( 'word-wrap/word-wrap.php' ) ) {
         add_action( 'admin_notices', 'child_plugin_notice' );
 
@@ -25,10 +25,10 @@ function has_word_wrap() {
         }
     }
 }
-add_action( 'admin_init', '\ElegantSlider\has_word_wrap' );
+add_action( 'admin_init', '\ElegantSlider\hasWordWrap' );
 
-function child_plugin_notice(){
-    ?><div class="error"><p>Sorry, but Child Plugin requires the Parent plugin to be installed and active.</p></div><?php
+function showInstallErrorMessage(){
+    echo '<div class="error"><p>Sorry, but Elegant Slider requires Word Wrap to be installed and active.</p></div>';
 }
 
 function autoload($className) {
@@ -40,78 +40,6 @@ function autoload($className) {
 
 spl_autoload_register(__NAMESPACE__ . "\\autoload");
 
-/*
-    "WordPress Plugin Template" Copyright (C) 2015 Michael Simpson  (email : michael.d.simpson@gmail.com)
 
-    This following part of this file is part of WordPress Plugin Template for WordPress.
-
-    WordPress Plugin Template is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    WordPress Plugin Template is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Contact Form to Database Extension.
-    If not, see http://www.gnu.org/licenses/gpl-3.0.html
-*/
-
-$minimalRequiredPhpVersion = '5.4';
-
-/**
- * Check the PHP version and give a useful error message if the user's version is less than the required version
- * @return boolean true if version check passed. If false, triggers an error which WP will handle, by displaying
- * an error message on the Admin page
- */
-function noticePhpVersionWrong() {
-    global $minimalRequiredPhpVersion;
-    echo '<div class="updated fade">' .
-        __('Error: plugin "Rules Regulations" requires a newer version of PHP to be running.',  'wordwrap-elegant-slider').
-        '<br/>' . __('Minimal version of PHP required: ', 'wordwrap-elegant-slider') . '<strong>' . $minimalRequiredPhpVersion . '</strong>' .
-        '<br/>' . __('Your server\'s PHP version: ', 'wordwrap-elegant-slider') . '<strong>' . phpversion() . '</strong>' .
-        '</div>';
-}
-
-
-function PhpVersionCheck() {
-    global $minimalRequiredPhpVersion;
-    if (version_compare(phpversion(), $minimalRequiredPhpVersion) < 0) {
-        add_action('admin_notices', 'PhpVersionCheck');
-        return false;
-    }
-    return true;
-}
-
-
-/**
- * Initialize internationalization (i18n) for this plugin.
- * References:
- *      http://codex.wordpress.org/I18n_for_WordPress_Developers
- *      http://www.wdmac.com/how-to-create-a-po-language-translation#more-631
- * @return void
- */
-function i18n_init() {
-    $pluginDir = dirname(plugin_basename(__FILE__));
-    load_plugin_textdomain('wordwrap-elegant-slider', false, $pluginDir . '/languages/');
-}
-
-
-//////////////////////////////////
-// Run initialization
-/////////////////////////////////
-
-// First initialize i18n
-i18n_init();
-
-
-// Next, run the version check.
-// If it is successful, continue with initialization for this plugin
-if (PhpVersionCheck()) {
-    // Only load and run the init function if we know PHP version can parse it
-    include_once(__DIR__ . '/../word-wrap/word-wrap.php');
-    WordWrap::init(basename(__DIR__));
-}
+include_once(__DIR__ . '/../word-wrap/word-wrap.php');
+WordWrap::init(basename(__DIR__));
